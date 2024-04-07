b11638412cbd647fd837951a87c8604e0ae01854
Rohaandsome <rohaan2403@gmail.com>
2024-04-07 13:42:53 +0200

    Initial commit

---
+++ src/main/java/com/example/obligatorisk/ObligatoriskApplication.java
@@ -0,0 +1,13 @@
+package com.example.obligatorisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligatoriskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObligatoriskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/obligatorisk/ObligatoriskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obligatorisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligatoriskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
